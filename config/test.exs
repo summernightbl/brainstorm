c2b9da1efd80e68dab7a81087ab795f0cf32a35c
summernightbl <104312972+summernightbl@users.noreply.github.com>
2022-08-23 14:55:04 +0300

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,7 @@ config :brainstorm, Brainstorm.Repo,
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :quackbox, QuackboxWeb.Endpoint,
+config :brainstorm, BrainstormWeb.Endpoint,
   http: [port: 4002],
   server: false
 
