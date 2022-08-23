40dd5753443118ce144ea676d9d9f7d499dd1199
summernightbl <104312972+summernightbl@users.noreply.github.com>
2022-08-23 14:24:55 +0300

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -2,12 +2,19 @@ use Mix.Config
 
 # Configure your database
 config :brainstorm, Brainstorm.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USER"),
-  password: System.get_env("POSTGRES_PASSWORD"),
-  database: System.get_env("POSTGRES_DB_TEST"),
-  hostname: System.get_env("POSTGRES_HOST"),
-  pool: Ecto.Adapters.SQL.Sandbox
+-  username: "postgres",
+-  password: "postgres",
+-  database: "quackbox_test",
+-  hostname: "localhost",
++  adapter: Ecto.Adapters.Postgres,
++  username: System.get_env("POSTGRES_USER"),
++  password: System.get_env("POSTGRES_PASSWORD"),
++  database: System.get_env("POSTGRES_DB_TEST"),
++  hostname: System.get_env("POSTGRES_HOST"),
+   pool: Ecto.Adapters.SQL.Sandbox
+ 
+ # We don't run a server during test. If one is required,
+
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
